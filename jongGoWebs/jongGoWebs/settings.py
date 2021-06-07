b19dbee5606000e498e0fc770d1d5a864ad35fb5
dk <kamal1234@naver.com>
2021-06-07 21:01:08 +0900

    dJango Test

--- jongGoWebs/jongGoWebs/settings.py
+++ jongGoWebs/jongGoWebs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jongGoApp',        #서브앱 추가
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr' #한글
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'    # 아시아/서울
 
 USE_I18N = True
 
